a42e65c8d25ed08babbac7de0faf9eb04a1833c3
vir84nr <114455282+vir84nr@users.noreply.github.com>
2023-10-22 22:37:57 -0300

    Add files via upload

---
+++ js/alertas.js
@@ -0,0 +1,126 @@
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta1').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta3').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta4').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta5').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta6').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta7').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta8').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta9').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta10').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta11').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta12').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta13').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta14').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta15').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta16').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalert17').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta18').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta19').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
+window.addEventListener('load', function() {
+	this.document.getElementById('viewalerta20').addEventListener('click', function(){
+		alert('Tienda en construccion');
+	})
+	
+})
